2bb689f0394295a604e2325c3cc8eb6e4e441998
Cristian Quito <cristianandresquito@gmail.com>
2023-05-01 00:16:57 -0500

    feat: add Home.component

---
+++ src/components/Home.component.vue
@@ -0,0 +1,16 @@
+<template>
+    <img src="" alt="SignLingo-Logo">
+    <div class="option-list">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
