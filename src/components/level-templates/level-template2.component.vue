9201f48147c25e4ba4d87bcd8d3ae098d9d390b2
Tyrone <jhinta7@gmail.com>
2023-06-07 16:58:58 -0500

    creacion de level templates

---
+++ src/components/level-templates/level-template2.component.vue
@@ -0,0 +1,162 @@
+<script setup>
+
+defineProps({
+    src:{
+        type: String,
+        required: true,
+    },
+    rpta1:{
+        type: String,
+        required: true,
+    },
+    rpta2:{
+        type: String,
+        required: true,
+    },
+    rpta3:{
+        type: String,
+        required: true,
+    },
+    rpta4: {
+        type: String,
+        required: true,
+    },
+})
+
+</script>
+
+<template>
+
+
+
+<div class="block font-bold text-center p-4 border-round mb-3">
+    <div class="flex flex-wrap justify-content-center card-container blue-container gap-3">
+    <h1>Escoja el significado de la siguiente señal</h1>
+</div>
+<div class="flex flex-wrap justify-content-center card-container blue-container gap-3">
+    <img :src="src" alt="">
+</div>
+    <div class="flex flex-wrap justify-content-center card-container blue-container gap-3">
+            <div class="border-round w-15rem h-5rem text-white font-bold flex align-items-center justify-content-center">
+                <button class="button-68" role="button">{{rpta1}}</button>
+            </div>
+            <div class="border-round w-15rem h-5rem text-white font-bold flex align-items-center justify-content-center">
+                <button class="button-68" role="button">{{rpta2}}</button>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap justify-content-center card-container blue-container gap-3">
+            <div class="border-round w-15rem h-5rem text-white font-bold flex align-items-center justify-content-center">
+                <button class="button-68" role="button">{{rpta3}}</button>
+            </div>
+            <div class="border-round w-15rem h-5rem text-white font-bold flex align-items-center justify-content-center">
+                <button class="button-68" role="button">{{rpta4}}</button>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "level-template2",
+}
+</script>
+
+<style scoped>
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+img {
+    width: 300px;
+    height: 300px;
+    float: left;
+    margin-right: 10px;
+    border-radius: 20%;
+    border: black 5px solid;
+}
+
+h1{
+    font-size: 40px;
+}
+
+p {
+    font-style: normal;
+    font-weight: 900;
+    font-size: 20px;
+}
+
+.nav-item {
+    padding: 20px 20px;
+    cursor: pointer;
+    border-radius: 10px;
+    transition: background-color 0.2s, opacity 0.2s;
+    margin: 30px 10px;
+}
+
+.nav-item:hover{
+    background-color: rgba(71, 167, 255, 0.7);
+}
+
+.nav-item:active{
+   opacity: 0.4;
+}
+
+.container{
+    display: flex;
+    align-items: center;
+}
+
+/* CSS */
+.button-68 {
+  appearance: none;
+  backface-visibility: hidden;
+  background-color: #27ae60;
+  border-radius: 8px;
+  border-style: none;
+  box-shadow: rgba(39, 174, 96, .15) 0 4px 9px;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Inter,-apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: normal;
+  line-height: 1.5;
+  outline: none;
+  overflow: hidden;
+  padding: 13px 20px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transform: translate3d(0, 0, 0);
+  transition: all .3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+}
+
+.button-68:hover {
+  background-color: #1e8449;
+  opacity: 1;
+  transform: translateY(0);
+  transition-duration: .35s;
+}
+
+.button-68:active {
+  transform: translateY(2px);
+  transition-duration: .35s;
+}
+
+.button-68:hover {
+  box-shadow: rgba(39, 174, 96, .2) 0 6px 12px;
+}
+
+
+</style>
